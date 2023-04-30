e6f242d52dc8232b9179a2cf2d23e84d720b4194
Sofia Medina <medinasofiaa1@gmail.com>
2023-04-30 03:30:22 -0300

    actualizacion

--- algoritmo.js
+++ algoritmo.js
@@ -1,15 +1,37 @@
-const inputEntrada = document.querySelector ("#info-ingresada");
+const inputNombre = document.querySelector ("#info-ingresada");
+const inputEdad = document.querySelector("#edad-ingresada");
+const inputEmail = document.querySelector("#email-ingresado");
+const inputDni = document.querySelector("#dni-ingresado");
 const pNombreUsuario = document.querySelector("#nombre");
+const pEdadUsuario =document.querySelector("#edad");
+const pEmailUsuario= document.querySelector("#email");
+const pDniUsuario = document.querySelector("#dni");
 
 
 let nombreRecibido = "def nombre";
+let edadRecibida = "def edad";
+let emailRecibido = "def email";
+let dniRecibido = "def dni";
+
 
 
 function info() {
 
-    nombreRecibido = inputEntrada.value;
+
     pNombreUsuario.innerHTML = nombreRecibido;
+    pEdadUsuario.innerHTML= edadRecibida;
+    pEmailUsuario.innerHTML= emailRecibido;
+    pDniUsuario.innerHTML= dniRecibido;
+
+    nombreRecibido = inputNombre.value;
+    edadRecibida = inputEdad.value;
+    emailRecibido = inputEmail.value;
+    dniRecibido = inputDni.value;
 
 if (nombreRecibido)
-    pNombreUsuario.innerHTML = " welcome " + nombreRecibido;
+    pNombreUsuario.innerHTML = " Bienvenida " + nombreRecibido;
+    pEdadUsuario.innerHTML = "tu edad es " + edadRecibida;
+    pEmailUsuario.innerHTML= "tu email es " + emailRecibido;
+    pDniUsuario.innerHTML= " tu DNI es " + dniRecibido;
+
 }
